93644a8df91af2d889942a29c0893a806fa7364c
dorothy879 <dtakawira272@gmail.com>
2024-11-05 09:45:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,29 @@ const body = document.querySelector("body"),
         backDelay: 2000,
 
     })
+
+const sr = ScrollReveal({
+        origin: "top",
+        distance: "80px",
+        duration: 2000,
+        reset: true,
+    });
+
+    sr.reveal(".featured-name", { delay: 100});
+    sr.reveal(".text-info", { delay: 200});
+    sr.reveal(".text-btn", { delay: 200});
+    sr.reveal(".social_icons", { delay: 200});
+    sr.reveal(".featured-image", { delay: 320});
+
+
+    sr.reveal(".project-box", { interval: 200});
+
+    sr.reveal(".top-header", {});
+
+
+    const srLeft = ScrollReveal({
+        origin: "left",
+        distance: "80px",
+        duration: 2000,
+        reset: true,
+    })
